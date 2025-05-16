3f061f21ed58abd2c91072a9907824ec1a216b73
adm1njeh0 <52900923+adm1njeh0@users.noreply.github.com>
2025-05-16 17:09:56 +0900

    Create parameters.sh

---
+++ deployment/parameters.sh
@@ -0,0 +1,10 @@
+IDC_LOGIN_URL=https://d-9b670f89db.awsapps.com/start/
+REGION=ap-northeast-2
+TEAM_ACCOUNT=527954255340
+ORG_MASTER_PROFILE=OrgMaster
+TEAM_ACCOUNT_PROFILE=TeamAccount
+TEAM_ADMIN_GROUP="TEAM_Admins"
+TEAM_AUDITOR_GROUP="TEAM_Auditors"
+TAGS="project=iam-identity-center-team environment=prod"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:ap-northeast-2:527954255340:eventdatastore/d5917ae6-3730-4aa4-8346-32cd7d31acdb
+SECRET_NAME=team_secret
